66fd00d431923992118058ff3d7325264253fc1a
Henrique <henriquecpreto@gmail.com>
2026-05-02 11:04:16 -0300

    The program ask the user's name, retire the withespace from the str, capitalize the fisrt letter of each word, and show all it at the end!

--- str/hello-1.py
+++
@@ -1,5 +0,0 @@
-# Ask the user their name
-name = input("Hi! What's your name? ")
-
-#Show the user's name
-print("Hello,", name)
---
+++ str/hello-2.py
@@ -0,0 +1,11 @@
+# Ask the user their name
+name = input("Hi! What's your name? ")
+
+# Retire the whitespace from the str
+name = name.strip()
+
+# Capitalize the fisrt letter of each word
+name = name.title()
+
+#Show the user's name
+print("Hello,", name)
