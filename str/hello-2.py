6e8db9e6dca3c4f6bede00d90f2b1140cc03a6dc
Henrique <henriquecpreto@gmail.com>
2026-05-02 11:17:10 -0300

    The program make the same thing, but, it's most simplified!

--- str/hello-2.py
+++ str/hello-2.py
@@ -1,11 +1,5 @@
-# Ask the user their name
-name = input("Hi! What's your name? ")
+#Ask the user's name and store it in a variable called "name"
+name = input("What's your name? ")
 
-# Retire the whitespace from the str
-name = name.strip()
-
-# Capitalize the fisrt letter of each word
-name = name.title()
-
-#Show the user's name
+#Show the user's name using the variable.
 print("Hello,", name)
