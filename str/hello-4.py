25ad231a2f7a83a50b351827d9822ad4442ce752
Henrique <henriquecpreto@gmail.com>
2026-05-02 11:24:43 -0300

    The program ask the user's name, store in a variable, and show it at the end.

---
+++ str/hello-4.py
@@ -0,0 +1,5 @@
+# Ask the user their name, capitalize the fisrt letter of each word and Retire the whitespace from the str
+name = input("Hi! What's your name? ").title().strip()
+
+#Show the user's name
+print("Hello,", name)
